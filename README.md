f512e3b576b64aa4f8f7d7de1550a75c8b041bd5
Alberto Penas Amor <albertopeam@gmail.com>
2022-01-27 23:49:51 +0100

    added zokrates verifier.sol

--- README.md
+++ README.md
@@ -19,3 +19,31 @@ The capstone will build upon the knowledge you have gained in the course in orde
 * run the tests:
   * `cd eth-contracts`
   * `truffle test`
+
+* zokrates info:
+  * https://jspaint.app/#local:1ecabf8d3ef4
+  * https://medium.com/extropy-io/zokrates-tutorial-with-truffle-41135a3fb754
+  * https://media.consensys.net/introduction-to-zksnarks-with-examples-3283b554fc3b
+
+  ![Alt text](./../docs/zokrates.info.png "zokrates")
+
+  General:
+  We can think of this more concretely as having a program, denoted C, taking two inputs: C(x, w). The input x is the public input, and w is the secret witness input. The output of the program is boolean, i.e. either true or false. The goal then is given a specific public input x, prove that the prover knows a secret input w such that C(x,w) == true.
+    Program(public_input, witness_input) -> Bool
+      being known public_input
+      being supposedly know witness
+      Program(public_input, witness_input) = true -> prover knows witness
+
+  Detail:
+  zk-snarks: consists of three algorithms G, P, V
+  * G: Key generator, takes a secret parameter lambda and a program C and generates two publicly available keys, a proving key pk, and a verification key vk. These keys are public parameters that only need to be generated once for a given program C
+    (pk, vk) = G(λ, C)
+    (proving_key, verification_key) = KeyGenerator(secret_param, program)
+  * The prover P takes as input the proving key pk, a public input x and a private witness w. The algorithm generates a proof prf = P(pk, x, w) that the prover knows a witness w and that the witness satisfies the program.
+    prf = P(pk, x, w)
+    proof = Prover_Algorithm(proving_key, public input, witness)
+  
+  * The verifier V computes V(vk, x, prf) which returns true if the proof is correct, and false otherwise. Thus this function returns true if the prover knows a witness w satisfying C(x,w) == true.
+    verification = V(vk, x, prf)
+    verification = V(verification_key, x, proof)
+      verification = true => prover knows witness
---
+++ zokrates/code/square/verifier.sol
@@ -0,0 +1,202 @@
+// This file is MIT Licensed.
+//
+// Copyright 2017 Christian Reitwiessner
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+pragma solidity ^0.8.0;
+library Pairing {
+    struct G1Point {
+        uint X;
+        uint Y;
+    }
+    // Encoding of field elements is: X[0] * z + X[1]
+    struct G2Point {
+        uint[2] X;
+        uint[2] Y;
+    }
+    /// @return the generator of G1
+    function P1() pure internal returns (G1Point memory) {
+        return G1Point(1, 2);
+    }
+    /// @return the generator of G2
+    function P2() pure internal returns (G2Point memory) {
+        return G2Point(
+            [10857046999023057135944570762232829481370756359578518086990519993285655852781,
+             11559732032986387107991004021392285783925812861821192530917403151452391805634],
+            [8495653923123431417604973247489272438418190587263600148770280649306958101930,
+             4082367875863433681332203403145435568316851327593401208105741076214120093531]
+        );
+    }
+    /// @return the negation of p, i.e. p.addition(p.negate()) should be zero.
+    function negate(G1Point memory p) pure internal returns (G1Point memory) {
+        // The prime q in the base field F_q for G1
+        uint q = 21888242871839275222246405745257275088696311157297823662689037894645226208583;
+        if (p.X == 0 && p.Y == 0)
+            return G1Point(0, 0);
+        return G1Point(p.X, q - (p.Y % q));
+    }
+    /// @return r the sum of two points of G1
+    function addition(G1Point memory p1, G1Point memory p2) internal view returns (G1Point memory r) {
+        uint[4] memory input;
+        input[0] = p1.X;
+        input[1] = p1.Y;
+        input[2] = p2.X;
+        input[3] = p2.Y;
+        bool success;
+        assembly {
+            success := staticcall(sub(gas(), 2000), 6, input, 0xc0, r, 0x60)
+            // Use "invalid" to make gas estimation work
+            switch success case 0 { invalid() }
+        }
+        require(success);
+    }
+
+
+    /// @return r the product of a point on G1 and a scalar, i.e.
+    /// p == p.scalar_mul(1) and p.addition(p) == p.scalar_mul(2) for all points p.
+    function scalar_mul(G1Point memory p, uint s) internal view returns (G1Point memory r) {
+        uint[3] memory input;
+        input[0] = p.X;
+        input[1] = p.Y;
+        input[2] = s;
+        bool success;
+        assembly {
+            success := staticcall(sub(gas(), 2000), 7, input, 0x80, r, 0x60)
+            // Use "invalid" to make gas estimation work
+            switch success case 0 { invalid() }
+        }
+        require (success);
+    }
+    /// @return the result of computing the pairing check
+    /// e(p1[0], p2[0]) *  .... * e(p1[n], p2[n]) == 1
+    /// For example pairing([P1(), P1().negate()], [P2(), P2()]) should
+    /// return true.
+    function pairing(G1Point[] memory p1, G2Point[] memory p2) internal view returns (bool) {
+        require(p1.length == p2.length);
+        uint elements = p1.length;
+        uint inputSize = elements * 6;
+        uint[] memory input = new uint[](inputSize);
+        for (uint i = 0; i < elements; i++)
+        {
+            input[i * 6 + 0] = p1[i].X;
+            input[i * 6 + 1] = p1[i].Y;
+            input[i * 6 + 2] = p2[i].X[1];
+            input[i * 6 + 3] = p2[i].X[0];
+            input[i * 6 + 4] = p2[i].Y[1];
+            input[i * 6 + 5] = p2[i].Y[0];
+        }
+        uint[1] memory out;
+        bool success;
+        assembly {
+            success := staticcall(sub(gas(), 2000), 8, add(input, 0x20), mul(inputSize, 0x20), out, 0x20)
+            // Use "invalid" to make gas estimation work
+            switch success case 0 { invalid() }
+        }
+        require(success);
+        return out[0] != 0;
+    }
+    /// Convenience method for a pairing check for two pairs.
+    function pairingProd2(G1Point memory a1, G2Point memory a2, G1Point memory b1, G2Point memory b2) internal view returns (bool) {
+        G1Point[] memory p1 = new G1Point[](2);
+        G2Point[] memory p2 = new G2Point[](2);
+        p1[0] = a1;
+        p1[1] = b1;
+        p2[0] = a2;
+        p2[1] = b2;
+        return pairing(p1, p2);
+    }
+    /// Convenience method for a pairing check for three pairs.
+    function pairingProd3(
+            G1Point memory a1, G2Point memory a2,
+            G1Point memory b1, G2Point memory b2,
+            G1Point memory c1, G2Point memory c2
+    ) internal view returns (bool) {
+        G1Point[] memory p1 = new G1Point[](3);
+        G2Point[] memory p2 = new G2Point[](3);
+        p1[0] = a1;
+        p1[1] = b1;
+        p1[2] = c1;
+        p2[0] = a2;
+        p2[1] = b2;
+        p2[2] = c2;
+        return pairing(p1, p2);
+    }
+    /// Convenience method for a pairing check for four pairs.
+    function pairingProd4(
+            G1Point memory a1, G2Point memory a2,
+            G1Point memory b1, G2Point memory b2,
+            G1Point memory c1, G2Point memory c2,
+            G1Point memory d1, G2Point memory d2
+    ) internal view returns (bool) {
+        G1Point[] memory p1 = new G1Point[](4);
+        G2Point[] memory p2 = new G2Point[](4);
+        p1[0] = a1;
+        p1[1] = b1;
+        p1[2] = c1;
+        p1[3] = d1;
+        p2[0] = a2;
+        p2[1] = b2;
+        p2[2] = c2;
+        p2[3] = d2;
+        return pairing(p1, p2);
+    }
+}
+
+contract Verifier {
+    using Pairing for *;
+    struct VerifyingKey {
+        Pairing.G1Point alpha;
+        Pairing.G2Point beta;
+        Pairing.G2Point gamma;
+        Pairing.G2Point delta;
+        Pairing.G1Point[] gamma_abc;
+    }
+    struct Proof {
+        Pairing.G1Point a;
+        Pairing.G2Point b;
+        Pairing.G1Point c;
+    }
+    function verifyingKey() pure internal returns (VerifyingKey memory vk) {
+        vk.alpha = Pairing.G1Point(uint256(0x0800ad9888a3b1f2b191bf5939a412601c47239a0383df3af55e1d517428bd40), uint256(0x151e42cf42004311e2173fc59e3baf9ad39a087ddd7131006cfa2804b442f95e));
+        vk.beta = Pairing.G2Point([uint256(0x0f427e89cffb4efe29d5bedebd80ea63a08851aa135347a06819fa54c422ef5d), uint256(0x18bbf11c1a56e0c05ac3f8052b059bfb152f96b0364bf7dfd082ef5c6b23423b)], [uint256(0x2b38a118d75c39dfa4f6396f5ccbc33517475c567b303d8e4ab6c0c96503b193), uint256(0x1ac43b902c8e55608237dbda8ece19be7794b2dd933bb5776f365ae0b3f143f3)]);
+        vk.gamma = Pairing.G2Point([uint256(0x263d932a4b58a33bf73ce1fd1acd0f2f52c6b9a93ef01a3993ee18ab2945620c), uint256(0x274dcfd858a4cfc42a58086148609574bb63ce673c0501bad8c875cb2e379693)], [uint256(0x2b69953171cc5d1a96b5e08cef71f7b8b5ac0aeb81b812c92a9bf13e6fdd831e), uint256(0x2c32f40025cb46c1f6aef926f2b4fc8a97d408ed721f27e986445a530de1278d)]);
+        vk.delta = Pairing.G2Point([uint256(0x19b96f92ae9865aceee2af92c3a5f8670a56df93576f66b46dd9a0ac6ac92190), uint256(0x19adbeb6415e568e061bcb8aa83371eaefbf83a239d1ea3fde079a128fc21418)], [uint256(0x27e7cca73f8467ec7d8426abe1412ad1b46b70536f9f853c995c71cb0b55e7a8), uint256(0x0de4a5fc9fedac7d034ea39ac6478128b853e14bc0f845505631949fa9f7cb87)]);
+        vk.gamma_abc = new Pairing.G1Point[](3);
+        vk.gamma_abc[0] = Pairing.G1Point(uint256(0x0d03de1bd969fddf521e636d72747989ce137cbb50ac89be799a6474f2e2fc57), uint256(0x024b7ee8caa72d9fa87bba9d15490d5ad7552a4e897989a2a7bc8563aec7f451));
+        vk.gamma_abc[1] = Pairing.G1Point(uint256(0x1b852780fa965e34a8e57a83480538481eb0be72d29a3e4acc45465dfacba900), uint256(0x2efefb0de490aab09869eb7ee825a1a4375455e361aaee5d99bed0c6fc1a94a9));
+        vk.gamma_abc[2] = Pairing.G1Point(uint256(0x072fc142f58f92aa2c6c6fb78be0f6a2b40393430e0aed13a58bfd6d40467fe8), uint256(0x1e8d3a5734ec5903300117e0748479f1ad7e4c0659441a4709a418844f455fae));
+    }
+    function verify(uint[] memory input, Proof memory proof) internal view returns (uint) {
+        uint256 snark_scalar_field = 21888242871839275222246405745257275088548364400416034343698204186575808495617;
+        VerifyingKey memory vk = verifyingKey();
+        require(input.length + 1 == vk.gamma_abc.length);
+        // Compute the linear combination vk_x
+        Pairing.G1Point memory vk_x = Pairing.G1Point(0, 0);
+        for (uint i = 0; i < input.length; i++) {
+            require(input[i] < snark_scalar_field);
+            vk_x = Pairing.addition(vk_x, Pairing.scalar_mul(vk.gamma_abc[i + 1], input[i]));
+        }
+        vk_x = Pairing.addition(vk_x, vk.gamma_abc[0]);
+        if(!Pairing.pairingProd4(
+             proof.a, proof.b,
+             Pairing.negate(vk_x), vk.gamma,
+             Pairing.negate(proof.c), vk.delta,
+             Pairing.negate(vk.alpha), vk.beta)) return 1;
+        return 0;
+    }
+    function verifyTx(
+            Proof memory proof, uint[2] memory input
+        ) public view returns (bool r) {
+        uint[] memory inputValues = new uint[](2);
+        
+        for(uint i = 0; i < input.length; i++){
+            inputValues[i] = input[i];
+        }
+        if (verify(inputValues, proof) == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
